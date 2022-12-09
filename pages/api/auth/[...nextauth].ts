4173e8a3dc92a9f2a6054604d5a50af728c1ad9e
Nathan Flacher <nathan.flacher.pro@gmail.com>
2022-12-09 01:10:30 +0100

    try to fix error of TS

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,29 +7,34 @@ const NextAuthOptions: NextAuthOptions = {
     },
     providers: [
         CredentialsProvider({
-            name: "",
             id: "",
             type: "credentials",
+            name: "Credentials",
             credentials: {
                 email: { label: "Email", type: "text", placeholder: "exemple@gmail.com" },
                 password: { label: "Mot de passe", type: "password" }
             },
-            authorize (credentials, req) {
-                const {email, password} = credentials as {
-                    email: string,
-                    password: string
-                }
-                if (email !== process.env.ADMIN_EMAIL && password !== process.env.ADMIN_PASSWORD) {
-                    throw new Error("invalid credentials")
-                }
-                return {id: 1, email: process.env.ADMIN_EMAIL, password: process.env.ADMIN_PASSWORD}
+            async authorize(credentials) {
+                const { email, password } = credentials as {
+                    email: string;
+                    password: string;
+                };
+                if (email !== process.env.ADMIN_EMAIL || password !== process.env.ADMIN_PASSWORD) {
+                    throw new Error("invalid credentials");
+                };
+                return {
+                    id: 1,
+                    email: process.env.ADMIN_EMAIL,
+                    password: process.env.ADMIN_PASSWORD,
+                    role: "admin"
+                } as any;
             }
         })
     ],
     pages: {
         signIn: "/login",
         signOut: '/logout'
-    }
+    };
 }
 
 export default NextAuth(NextAuthOptions)

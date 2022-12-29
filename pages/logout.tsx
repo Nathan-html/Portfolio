8899a2e3843ebc1f50ababf6f605685f8a4d91b6
Nathan Flacher <nathan.flacher.pro@gmail.com>
2022-12-29 18:31:07 +0100

    fix logout

---
+++ pages/logout.tsx
@@ -0,0 +1,8 @@
+import {NextPage} from "next";
+import LoadingScreen from "../components/loadingScreen";
+
+const Logout: NextPage = () => {
+    return <LoadingScreen />
+}
+
+export default Logout
